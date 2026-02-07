53df7cbf80115453b9d34a635f064502d8ff6711
Sagrika Trehan <trehansagrika0897@gmail.com>
2026-02-07 18:56:40 +0530

    updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,21 +7,18 @@ const User = require('../models/user.js');
 
 const saltRounds = 12;
 
-router.post('/sign-up', async (req, res) => {
+router.post("/sign-up", async (req, res) => {
   try {
     const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) return res.status(409).json({ err: "Username already taken." });
 
-    if (userInDatabase) {
-      return res.status(409).json({ err: 'Username already taken.' });
-    }
-    console.log(req.body);
     const user = await User.create({
       username: req.body.username,
-      hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
+      email: req.body.email, 
+      password: bcrypt.hashSync(req.body.password, saltRounds),
     });
 
     const payload = { username: user.username, _id: user._id };
-
     const token = jwt.sign({ payload }, process.env.JWT_SECRET);
 
     res.status(201).json({ token });
